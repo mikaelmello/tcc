51414e1822419391424e08f1d19a074fe746ad6a
Mikael Mello <git@mikaelmello.com>
2020-10-26 21:34:44 -0300

    Working solution for onnx without GPU

--- simulation/src/runners/python/onnx/off/main.py
+++ simulation/src/runners/python/onnx/off/main.py
@@ -1,39 +1,70 @@
-import numpy
 import time
-import onnxruntime as rt
+import os
+import argparse
+import json
+from classifier import Classifier
+from zipfile import ZipFile
 
 
-class Classifier:
-    def __init__(self):
-        self.classifier = rt.InferenceSession("model.onnx")
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-path", "-m", required=True, type=str)
+    parser.add_argument("--input-path", "-i", required=True, type=str)
+    parser.add_argument("--output-path", "-o", required=True, type=str)
 
-    def classify(self, values: list):
-        scaled = [[0, 0, 0, 0, 0, 0, 0]]
-        scale = [
-            119.83552861,
-            1.42122914,
-            1.31913874,
-            12.90645335,
-            1283.0616716,
-            45.84544104,
-            11.46346616,
-        ]
-        mean = [
-            1.67900462e02,
-            3.11090588e00,
-            4.43886897e00,
-            4.27830029e01,
-            4.23752228e03,
-            9.29964257e01,
-            3.18708094e01,
-        ]
-        for i in range(7):
-            scaled[0][i] = (values[i] - mean[i]) / scale[i]
+    args = vars(parser.parse_args())
+    return args
 
-        input_name = self.classifier.get_inputs()[0].name
-        prediction = self.classifier.run(None, {input_name: scaled})
 
-        return int(prediction[0][0])
+class Input:
+    def __init__(self, input_path):
+        self.input_zip = ZipFile(input_path)
+        self.to_open = self.input_zip.namelist()
+        self.open = False
+
+    def get_next(self):
+        while True:
+            if not self.open:
+                if not self.to_open:
+                    return
+
+                filename = self.to_open[-1]
+                self.to_open.pop()
+                content = self.input_zip.read(filename)
+                self.cur_file = json.loads(content.decode("utf8"))
+                self.open = True
+                self.cur_index = 0
+
+            inputs = self.cur_file["inputs"]
+            count = self.cur_file["count"]
+            payload = inputs[self.cur_index]
+
+            self.cur_index += 1
+            if self.cur_index == count:
+                self.open = False
+
+            yield payload
+
+
+class Output:
+    def __init__(self, output_path):
+        self.output_path = output_path
+        self.results = []
+
+    def register_result(self, td, output, inp):
+        output_payload = {
+            "iid": inp["id"],
+            "o": output,
+            "eo": inp["expected_output"],
+            "d": td,
+            "du": "us",
+        }
+
+        self.results.append(output_payload)
+
+    def save(self):
+        with open(self.output_path, "w") as f:
+            json.dump(self.results, f, separators=(",", ":"))
 
 
 def classify(classifier, data):
@@ -46,6 +77,24 @@ def classify(classifier, data):
 
 
 if __name__ == "__main__":
-    classifier = Classifier()
-    for i in range(100000):
-        print(classify(classifier, [4, 4, 4, 4, 4, 4, 4]))
+    args = parse_args()
+
+    model_path = os.path.join(args["model_path"], "model.onnx")
+    input_path = args["input_path"]
+    output_path = args["output_path"]
+
+    classifier = Classifier(model_path)
+    data = Input(input_path)
+    output = Output(output_path)
+
+    count = 0
+    for inp in data.get_next():
+        if count % 1000 == 0:
+            print(f"\r{count} registered...", end="")
+        result = classify(classifier, inp["data"])
+        output.register_result(result[1], result[0], inp)
+        count += 1
+
+    print("\nSaving!")
+    output.save()
+    print("Done!")
