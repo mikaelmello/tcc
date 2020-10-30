2824339fb7cc6cdcf726e67961806fbf7debde7f
Mikael Mello <git@mikaelmello.com>
2020-10-29 21:50:54 -0300

    Remove useless method

--- simulation/src/runners/java/onnx/off/src/main/java/br/unb/cic/Classifier.java
+++ simulation/src/runners/java/onnx/off/src/main/java/br/unb/cic/Classifier.java
@@ -33,19 +33,4 @@ public class Classifier {
             return 2;
         }
     }
-
-    private static float[][] scaler(float[] values) {
-        assert values.length == 7;
-
-        float[][] scaled = { { 0, 0, 0, 0, 0, 0, 0 } };
-        double[] scale = { 119.83552861, 1.42122914, 1.31913874, 12.90645335, 1283.0616716, 45.84544104, 11.46346616 };
-        double[] mean = { 1.67900462e+02, 3.11090588e+00, 4.43886897e+00, 4.27830029e+01, 4.23752228e+03, 9.29964257e+01,
-                3.18708094e+01 };
-
-        for (int i = 0; i < 7; i++) {
-            scaled[0][i] = (float) ((values[i] - mean[i]) / scale[i]);
-        }
-
-        return scaled;
-    }
 }
