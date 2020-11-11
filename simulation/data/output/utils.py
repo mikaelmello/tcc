91b14daa60153b00a349c1f007356192a17489fa
Mikael Mello <git@mikaelmello.com>
2020-11-11 03:01:00 -0300

    Add utils to generate plots

---
+++ simulation/data/output/utils.py
@@ -0,0 +1,47 @@
+import os
+from os import path
+from pathlib import Path
+import json
+import matplotlib.pyplot as plt
+import pandas as pd
+
+DIR = Path(__file__).parent.absolute()
+
+json_files = [f for f in os.listdir(DIR) if f.endswith(".json")]
+
+expected_delta_unit = "us"
+
+runs = []
+for filename in json_files:
+    props = filename.split(".")[0]
+    lang, lib, gpu = props.split("_")
+    with open(path.join(DIR, filename), "r") as f:
+        parsed = json.load(f)
+        for idx, run in enumerate(parsed):
+            run["lang"] = lang
+            run["lib"] = lib
+            run["gpu"] = gpu
+            run["cmb"] = f"{lang},{lib},{gpu}"
+            run["i"] = idx
+
+            if run["du"] != expected_delta_unit:
+                print(f"{run} does not have expected du us")
+
+            run.pop("du")
+            run.pop("iid")
+            runs.append(run)
+
+df = pd.DataFrame(runs)
+
+
+def plot(queries=[], groupby=["lang", "lib", "gpu"]):
+    fig, ax = plt.subplots()
+
+    ddf = df
+    for i in queries:
+        ddf = ddf.query(i)
+
+    for key, grp in ddf.groupby(groupby):
+        ax = grp.plot(ax=ax, kind="line", x="i", y="d", label=key)
+
+    plt.show()
